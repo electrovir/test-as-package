03885a8d27e4d74c7d7870162075a98e5d2654d8
electrovir <electrovir@users.noreply.github.com>
2025-07-18 14:24:25 +0000

    [patch] use underscroes in env var names

--- src/package-being-tested-env-names.ts
+++ src/package-being-tested-env-names.ts
@@ -2,9 +2,9 @@
  * The name of the env var that, when a shell command is wrapped in `test-as-package`, will be
  * populated a JSON stringified array of all possible bin names for the package under test.
  */
-export const packageBeingTestedBinNames = 'TEST-AS-PACKAGE-BIN-NAMES';
+export const packageBeingTestedBinNames = 'TEST_AS_PACKAGE_BIN_NAMES';
 /**
  * The name of the env var that, when a shell command is wrapped in `test-as-package`, will be
  * populated with a path to the package under test's bin file's folder.
  */
-export const packageBeingTestedInstallationBinDirPath = 'TEST-AS-PACKAGE-INSTALLATION-BIN-DIR-PATH';
+export const packageBeingTestedInstallationBinDirPath = 'TEST_AS_PACKAGE_INSTALLATION_BIN_DIR_PATH';
